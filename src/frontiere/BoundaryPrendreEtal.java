5bb737990ca3077b2c9b2adb75dd85f46aa7e03a
Myst <yoannyo77@gmail.com>
2025-03-12 11:56:48 +0100

    Fait jusqu'a Boundary PrendreEtal

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,7 +10,24 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis desolee " + nomVendeur +
+					" mais il faut etre un habitant de notre village pour commercer ici.");
+		} else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un etal.");
+			
+			if (!controlPrendreEtal.resteEtals()) {
+				System.out.println("Desolee " + nomVendeur + 
+						" je n'ai plus d'etal qui ne soit pas deja occupe.");
+			} else {
+				this.installerVendeur(nomVendeur);
+				System.out.println("C'est parfait, il me reste un etal pour vous!");
+				System.out.println("Il me faudrait quelques renseignements:");
+				
+				String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre?");
+				int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre?");
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
