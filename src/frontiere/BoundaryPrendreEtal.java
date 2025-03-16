0372fc6b0f225d8888e147a9a79adef4ce4ab8d2
Myst <yoannyo77@gmail.com>
2025-03-16 10:07:59 +0100

    PrendreEtal et TrouverEtalVendeur fait

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -11,31 +11,29 @@ public class BoundaryPrendreEtal {
 
 	public void prendreEtal(String nomVendeur) {
 		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
-			System.out.println("Je suis desolee " + nomVendeur +
-					" mais il faut etre un habitant de notre village pour commercer ici.");
+			System.out.println("Je suis desolee " + nomVendeur
+					+ " mais il faut etre un habitant de notre village pour commercer ici.");
 		} else {
 			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un etal.");
-			
+
 			if (!controlPrendreEtal.resteEtals()) {
-				System.out.println("Desolee " + nomVendeur + 
-						" je n'ai plus d'etal qui ne soit pas deja occupe.");
+				System.out.println("Desolee " + nomVendeur + " je n'ai plus d'etal qui ne soit pas deja occupe.");
 			} else {
 				this.installerVendeur(nomVendeur);
-				System.out.println("C'est parfait, il me reste un etal pour vous!");
-				System.out.println("Il me faudrait quelques renseignements:");
-				
-				String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre?");
-				int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre?");
-				
-				int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
-				if (numeroEtal != -1) {
-					
-				}
 			}
 		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un etal pour vous!");
+		System.out.println("Il me faudrait quelques renseignements:");
+
+		String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre?");
+
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit) + 1;
+		if (numeroEtal != -1) {
+			System.out.println("Le vendeur " + nomVendeur + " s'est installe a l'etal no " + numeroEtal);
+		}
 	}
 }
